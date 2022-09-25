39336a661e9121947cd0429dd731be5bd95e298a
Josep Palomino <jfpalomino.22@gmail.com>
2022-09-25 10:48:11 -0500

    PreEntrega1+Palomino v2.0

--- src/components/CartWidget/CartWidget.jsx
+++ src/components/CartWidget/CartWidget.jsx
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 
 const CartWidget = () => {
+  const [contador, setContador] = useState(0);
   return (
-    <>
-      <i class="fa-solid fa-cart-shopping p-2"></i>
-    </>
+    <div>
+      <button
+        className="bg-transparent hover:border-transparent active:text-gray-600"
+        onClick={() => setContador(contador + 1)}
+      >
+        <i className="fa-solid fa-cart-shopping p-2"></i>
+      </button>
+      <span>{contador}</span>
+    </div>
   );
 };
 
