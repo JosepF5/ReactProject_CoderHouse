a612ee2c7bea8c3e85b7839bdda22a3c3740dc1f
Josep Palomino <jfpalomino.22@gmail.com>
2022-10-08 21:10:43 -0500

    PreEntrega2+Palomino v1.0

---
+++ src/components/ItemDetailContainer/ItemDetailContainer.jsx
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from "react";
+import ItemDetail from "../ItemDetail/ItemDetail";
+import { useParams } from "react-router-dom";
+import { getProducts, getProductById } from "../../utils/Mock";
+import Loader from "../Loader/Loader";
+const ItemDetailContainer = () => {
+  const [item, setItem] = useState({}); 
+  const [isLoading, setLoading] = useState(false);
+  const { idProd } = useParams();
+
+  useEffect(() => {
+    getProductById(idProd)
+      .then((res) => setItem(res))
+      .catch((err) => console.log(err))
+      .finally(() => setLoading(true));
+  }, []);
+  return (
+    <div>
+      {isLoading ? (
+        <ItemDetail item={item} />
+      ) : (
+        <Loader />
+      )}
+    </div>
+  ); 
+};
+
+export default ItemDetailContainer;
